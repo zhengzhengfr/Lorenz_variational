0adeb652e979c9f87ded1a63c6be04eea711e180
郑铮 <zhengzheng@tsf-492-wpa-4-078.epfl.ch>
2022-06-13 17:45:29 +0200

    seperate functions and programs -done

---
+++ Functions/DNS.m
@@ -0,0 +1,12 @@
+% DNS integration of the Lorenz system 
+% Zheng Zheng, June 2022
+
+%% Solving Lorenz system
+function [t,xyz] = DNS(tmax,dt,x0)
+    % Create times with dt
+    tspan = linspace(0, tmax, tmax/dt);
+    % Solve equation system
+    [t,xyz]=ode45(@Lorenz_equation,tspan,x0);
+end
+
+
---
+++ Functions/Lorenz_equation.m
@@ -0,0 +1,10 @@
+% DNS integration of the Lorenz system 
+% Zheng Zheng, June 2022
+
+% Differential equation for the Lorenz system
+function xprime = Lorenz_equation(~,x)
+    sig = 10;
+    beta = 8/3;
+    rho = 28; % 20 for non-chaotic behaviour
+    xprime=[-sig*x(1) + sig*x(2); rho*x(1) - x(2) - x(1)*x(3); -beta*x(3) + x(1)*x(2)];
+end
