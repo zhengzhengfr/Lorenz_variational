1a839b0ae1ffd6701fd82b3179f57e7aa6bb5fcc
郑铮 <zhengzheng@tsf-492-wpa-6-254.epfl.ch>
2022-06-22 11:45:10 +0200

    implicit and explicit works

--- Program/LorenzSystem.m
+++ Program/LorenzSystem.m
@@ -68,58 +68,58 @@ JJ = [];
 tt = [];
 figure;
 tau = 0;
-for i = 1:200
-    for j = 1:100
+for i = 1:2000
+    for j = 1:200
         tau = tau + d_tau;
         % G = linear + nonlinear terms in spectral
         [G1, G2, G3] = adjoint(x_hat, y_hat, z_hat, rho, sig, beta, T, k);
         T = update_period(x_hat, y_hat, z_hat, r1, r2, r3, T, k, d_tau);
 %         %explicit, this works, d_tau should be 0.0001, check dealising 
-%         x_hat = dealising(x_hat + G1*d_tau);
-%         y_hat = dealising(y_hat + G2*d_tau);
-%         z_hat = dealising(z_hat + G3*d_tau);
+        x_hat = dealising(x_hat + G1*d_tau);
+        y_hat = dealising(y_hat + G2*d_tau);
+        z_hat = dealising(z_hat + G3*d_tau);
         % explicit test 
-        for j = 1:length(k)
-            % Linear terms in spectral 
-            % For  x_hat
-            term_xl1(j) = -( sig^2 + rho^2 + (4*(pi^2)*(k(j)^2))/(T^2)  );
-            term_xl2(j) = ( sig^2 + rho+ ( 2*pi*k(j)*(rho - sig))*complex(0, 1)/T );
-            L1(j) = term_xl1(j)*x_hat(j) + term_xl2(j)*y_hat(j);
-            % For  y_hat
-            term_yl1(j) = ( sig^2 + rho+ ( 2*pi*k(j)*(sig - rho ))*complex(0, 1)/T  );
-            term_yl2(j) = -( sig^2 + 1 + (4*(pi^2)*(k(j)^2))/(T^2) );
-            L2(j) = term_yl1(j)*x_hat(j) + term_yl2(j)*y_hat(j);
-            % For  z_hat
-            term_zl1(j) = -( beta^2 + (4*(pi^2)*(k(j)^2))/(T^2) );
-            L3(j) = term_zl1(j)*z_hat(j);
-            
-            % Non-linear terms in spectral 
-            N1(j) = G1(j) - L1(j);
-            N2(j) = G2(j) - L2(j);
-            N3(j) = G3(j) - L3(j);
-
-            % implicit test, d_tau = 0.0001, works but slow 
-            % Matrix to be inversed 
-            A = 1 - d_tau*term_xl1(j);
-            B = -d_tau*term_xl2(j);
-            C = -d_tau*term_yl1(j);
-            D = 1 - d_tau*term_yl2(j);
-            E = d_tau*N1(j) + x_hat(j);
-            F = d_tau*N2(j) + y_hat(j);
-            % new x,y,z in spectral 
-            [x_new(j), y_new(j)] = inverse_matrix(A, B, C, D, E, F);
-            z_new(j) = (z_hat(j) + d_tau*N3(j))/(1 - d_tau* term_zl1(j));
-        end
-        % If linear terms treated explicitly
-%         x_new = x_hat.*(1 + d_tau*term_xl1) + y_hat.*term_xl2*d_tau + d_tau*N1;
-%         y_new = x_hat.*term_yl2*d_tau + y_hat.*(1 + d_tau*term_yl2) + d_tau*N2;
-%         z_new = z_hat.*(1 + d_tau*term_zl1)                                                   + d_tau*N3;
-
-        x_hat = dealising(x_new);
-        y_hat = dealising(y_new);
-        z_hat = dealising(z_new);
-
-        [r1, r2, r3] = residual(x_hat, y_hat, z_hat, sig, beta, rho, T, k); % why this line cannot be after end
+%         for j = 1:length(k)
+%             % Linear terms in spectral 
+%             % For  x_hat
+%             term_xl1(j) = -( sig^2 + rho^2 + (4*(pi^2)*(k(j)^2))/(T^2)  );
+%             term_xl2(j) = ( sig^2 + rho+ ( 2*pi*k(j)*(rho - sig))*complex(0, 1)/T );
+%             L1(j) = term_xl1(j)*x_hat(j) + term_xl2(j)*y_hat(j);
+%             % For  y_hat
+%             term_yl1(j) = ( sig^2 + rho+ ( 2*pi*k(j)*(sig - rho ))*complex(0, 1)/T  );
+%             term_yl2(j) = -( sig^2 + 1 + (4*(pi^2)*(k(j)^2))/(T^2) );
+%             L2(j) = term_yl1(j)*x_hat(j) + term_yl2(j)*y_hat(j);
+%             % For  z_hat
+%             term_zl1(j) = -( beta^2 + (4*(pi^2)*(k(j)^2))/(T^2) );
+%             L3(j) = term_zl1(j)*z_hat(j);
+%             
+%             % Non-linear terms in spectral 
+%             N1(j) = G1(j) - L1(j);
+%             N2(j) = G2(j) - L2(j);
+%             N3(j) = G3(j) - L3(j);
+% 
+% %             % implicit test, d_tau = 0.0001, works but slow 
+% %             % Matrix to be inversed 
+% %             A = 1 - d_tau*term_xl1(j);
+% %             B = -d_tau*term_xl2(j);
+% %             C = -d_tau*term_yl1(j);
+% %             D = 1 - d_tau*term_yl2(j);
+% %             E = d_tau*N1(j) + x_hat(j);
+% %             F = d_tau*N2(j) + y_hat(j);
+% %             % new x,y,z in spectral 
+% %             [x_new(j), y_new(j)] = inverse_matrix(A, B, C, D, E, F);
+% %             z_new(j) = (z_hat(j) + d_tau*N3(j))/(1 - d_tau* term_zl1(j));
+%         end
+% %         %If linear terms treated explicitly
+% %         x_new = x_hat.*(1 + d_tau*term_xl1) + y_hat.*term_xl2*d_tau + d_tau*N1;
+% %         y_new = x_hat.*term_yl1*d_tau + y_hat.*(1 + d_tau*term_yl2) + d_tau*N2;
+% %         z_new = z_hat.*(1 + d_tau*term_zl1)                                                   + d_tau*N3;
+% 
+% %         x_hat = dealising(x_new);
+% %         y_hat = dealising(y_new);
+% %         z_hat = dealising(z_new);
+% 
+          [r1, r2, r3] = residual(x_hat, y_hat, z_hat, sig, beta, rho, T, k); 
     end
     x_phy = ifft( x_hat, 'symmetric');
     y_phy = ifft( y_hat, 'symmetric');
